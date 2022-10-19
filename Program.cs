a1c7003b8248e356f69f2ff9c7af681ac8c55fd5
Jovann Contreras <jcontreras@codestack.co>
2022-10-19 15:55:41 -0700

    created arrays

--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
 ﻿
+
+string[] Res = {"Chili's", "Sizzlers", "BJ's", "Olive Garden", "Outback", "Apple Bee's", "Payters", "Cheesecake Factory", "Texas Roadhouse", "Captain Crab"};
+string[] FastFood = {"Mcdonalds", "Burger King", "Subway", "Carls.Jr", "Wendy's", "Philly CheeseStake", "Jack in box", "Canes", "Chic Fil a", "PopEyes"};
+string[] Dessert = {"Basken Robins", "Sizzlers", "BJ's", "Olive Garden", "Outback", "Apple Bee's", "Payters", "Cheesecake Factory", "Texas Roadhouse", "Captain Crab"};
