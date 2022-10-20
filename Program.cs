ca31890184be8fe952ccb625650357a29da7c5d2
Kenzodiac <kennethfujimura@gmail.com>
2022-10-20 14:36:20 -0700

    Peer review by Kenneth Fujimura

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿
+﻿//Peer Reviewed By: Kenneth Fujimura
+//Everything looks good to me. Everything works without any bugs that I could find. The input validation works well. I liked how you were using the .Length function to set some of the parameters. That kind of thing is really good so in case you wanted to add more data in the future, it will require a lot less editing your code to accomodate it. Very good practice. The only thing I would change is:
+//- In the instructions, make it very clear what kind of input you want. Like telling the player to type the categories exactly as you wrote them. That's not totally necessary, but it's always good form to try and make your intructions to your users as clear as possible.
+//- Make sure you're following the submission guidelines. You're missing the comment information at the top of your VS Code.
+//But yeah, great job, man!
+
 Console.Clear();
 string[] Res = { "Chili's", "Sizzlers", "BJ's", "Olive Garden", "Outback", "Apple Bee's", "Payters", "Cheesecake Factory", "Texas Roadhouse", "Captain Crab" };
 string[] FastFood = { "Mcdonalds", "Burger King", "Subway", "Carls.Jr", "Wendy's", "Philly CheeseStake", "Jack in box", "Canes", "Chic Fil a", "PopEyes" };
